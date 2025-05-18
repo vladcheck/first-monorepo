12cf02ec4d34e0af367f2dcb296208297ff648c1
Vladimir Valekzhanin <valdemarcheck@yandex.ru>
2025-05-18 19:08:29 +0300

    Setup fractions module

---
+++ packages/fractions/index.js
@@ -0,0 +1,33 @@
+module.exports.Fraction = class Fraction {
+  constructor(nominator, denominator) {
+    if (denominator === 0) {
+      throw Error("Denominator equals zero");
+    }
+    this.nom = nominator;
+    this.den = denominator;
+  }
+
+  add(F) {
+    if (this.den === F.den) {
+      return new Fraction(this.nom + F.nom, this.den);
+    }
+  }
+
+  sub(F) {
+    if (this.den === F.den) {
+      return new Fraction(this.nom - F.nom, this.den);
+    }
+  }
+
+  mult(F) {
+    return new Fraction(this.nom * F.nom, this.den * F.den);
+  }
+
+  div(F) {
+    return new Fraction(this.nom * F.den, this.den * F.nom);
+  }
+
+  toString() {
+    return `${nom}/${den}`;
+  }
+};
