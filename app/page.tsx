4ca809035e51cefb853e91a76392b2ce5b193dcd
Vladimir Valekzhanin <valdemarcheck@yandex.ru>
2025-05-18 19:58:04 +0300

    Setup fractionsForm for test

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
+import FractionForm from "./FractionForm";
 
 export default function Page() {
   return (
     <>
       <h1>Hello, NextJS!</h1>
       <Image src="/image.jpeg" alt="Profile" width={100} height={100} />
+      <FractionForm />
     </>
   );
 }
