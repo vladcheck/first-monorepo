df25fa640deed3eb5314e736a9b93a2f58615c7b
vladcheck <valdemarcheck@yandex.ru>
2025-05-21 11:53:10 +0300

    Change the way first package is built

---
+++ packages/fractions/index.js
@@ -0,0 +1,33 @@
+define("index", ["require", "exports"], function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    class Fraction {
+        constructor(nominator, denominator) {
+            if (denominator === 0) {
+                throw Error("Denominator equals zero");
+            }
+            this.nom = nominator;
+            this.den = denominator;
+        }
+        add(F) {
+            if (this.den === F.den) {
+                return new Fraction(this.nom + F.nom, this.den);
+            }
+        }
+        sub(F) {
+            if (this.den === F.den) {
+                return new Fraction(this.nom - F.nom, this.den);
+            }
+        }
+        mult(F) {
+            return new Fraction(this.nom * F.nom, this.den * F.den);
+        }
+        div(F) {
+            return new Fraction(this.nom * F.den, this.den * F.nom);
+        }
+        toString() {
+            return `${this.nom}/${this.den}`;
+        }
+    }
+    exports.default = Fraction;
+});
